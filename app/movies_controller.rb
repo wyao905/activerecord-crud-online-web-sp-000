2fadb1d82e43d3913bf880261dd501a404e1e953
Warren Yao <warrenyca905@gmail.com>
2019-08-15 02:10:04 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,7 @@ def can_be_created_in_a_block(args = nil)
   # release_date == 1990
   
   Movie.create do |m|
-    if args
+    if !args
       m.title = "Home Alone"
       m.release_date = 1990
     end
